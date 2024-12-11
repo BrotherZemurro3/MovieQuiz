8cff9a33dfd96a493b50b582243ea9d83c98dade
BrotherZemurro3 <konevichenko@icloud.com>
2024-12-12 01:04:22 +0300

    Исправлены ошибки в тестах

--- MovieQuizTests/MovieQuizTests.swift
+++ MovieQuizTests/MovieQuizTests.swift
@@ -5,12 +5,12 @@
 //  Created by Дионисий Коневиченко on 12.12.2024.
 //
 
-import UIKit
-import Foundation
+
 import XCTest
 @testable import MovieQuiz
 final class MoviesLoaderTests: XCTestCase {
-    func testSuccessLoading() throws {
+    
+func SuccessLoadinTests() throws {
         let stubNetworkClient = StubNetworkClient(emulateError: false)
                 let loader = MoviesLoader(networkClient: stubNetworkClient)
 
@@ -31,11 +31,9 @@ final class MoviesLoaderTests: XCTestCase {
                 waitForExpectations(timeout: 1)
             }
 
-            func testFailureLoading() throws {
-                func testFailureLoading() throws {
-
-                    let stubNetworkClient = StubNetworkClient(emulateError: true)
-                    let loader = MoviesLoader(networkClient: stubNetworkClient)
+func FailureLoadingTests() throws {
+let stubNetworkClient = StubNetworkClient(emulateError: true)
+let loader = MoviesLoader(networkClient: stubNetworkClient)
 
 
                     let expectation = expectation(description: "Loading expectation")
@@ -104,3 +102,4 @@ final class MoviesLoaderTests: XCTestCase {
             }
         }
 }
+
--- MovieQuizUITests/MovieQuizUITests.swift
+++ MovieQuizUITests/MovieQuizUITests.swift
@@ -6,29 +6,26 @@
 //
 
 import XCTest
-import UIKit
-import Foundation
 @testable import MovieQuiz
- class MovieQuizUITests: XCTestCase {
-    
+final class MovieQuizUITests: XCTestCase {
     var app: XCUIApplication!
-    override func setUpWithError() throws {
-        try super.setUpWithError()
-        
-        app = XCUIApplication()
-        app.launch()
-        
-        // это специальная настройка для тестов: если один тест не прошёл
-        // то следующие тесты запускаться не будут; и правда, зачем ждать?
-        continueAfterFailure = false
-        
-    }
-    
-    override func tearDownWithError() throws {
-        try super.setUpWithError()
-        app.terminate()
-        app = nil
-    }
+
+       override func setUpWithError() throws {
+           try super.setUpWithError()
+
+           app = XCUIApplication()
+           app.launch()
+
+           continueAfterFailure = false
+       }
+
+       override func tearDownWithError() throws {
+           try super.tearDownWithError()
+
+           app.terminate()
+           app = nil
+       }
+    func testScreenCast() throws {}
     
     func testYesButton() {
         sleep(3)
@@ -59,48 +56,33 @@ import Foundation
         
         XCTAssertEqual(indexLabel.label, "2/10")
         
-        func testAlert() {
-            sleep(3)
-            for _ in 1...10 {
-                app.buttons["No"].tap()
-                sleep(3)
-            }
-            let alert = app.alerts["Game results"]
-            XCTAssertTrue(alert.exists)
-            XCTAssertTrue(alert.label == "Этот раунд окончен")
-            XCTAssertTrue(alert.buttons.firstMatch.label == "Сыграть еще раз")
-        }
-        
-        func testAlertDismiss() {
-            sleep(3)
-            for _ in 1...10 {
-                app.buttons["No"].tap()
-                sleep(3)
-            }
-            let alert = app.alerts["Game results"]
-            alert.buttons.firstMatch.tap()
-            sleep(3)
-            let indexLabel = app.staticTexts["Index"]
-            XCTAssertFalse(alert.exists)
-            XCTAssertTrue(indexLabel.label == "1/10")
-        }
-    }
-        @MainActor
-        func testExample() throws {
-            
-            let app = XCUIApplication()
-            app.launch()
-            
-        }
-        
-        @MainActor
-        func testLaunchPerformance() throws {
-            if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-                
-                measure(metrics: [XCTApplicationLaunchMetric()]) {
-                    XCUIApplication().launch()
+        func AlertTest() throws {
+                    sleep(3)
+                    for _ in 1...10 {
+                        app.buttons["No"].tap()
+                        sleep(3)
+                    }
+                    let alert = app.alerts["Game results"]
+                    XCTAssertTrue(alert.exists)
+                    XCTAssertTrue(alert.label == "Этот раунд окончен")
+                    XCTAssertTrue(alert.buttons.firstMatch.label == "Сыграть еще раз")
                 }
-            }
-        }
+        
+        func AlertDismissTest() throws {
+                    sleep(3)
+                    for _ in 1...10 {
+                        app.buttons["No"].tap()
+                        sleep(3)
+                    }
+                    let alert = app.alerts["Game results"]
+                    alert.buttons.firstMatch.tap()
+                    sleep(3)
+                    let indexLabel = app.staticTexts["Index"]
+                    XCTAssertFalse(alert.exists)
+                    XCTAssertTrue(indexLabel.label == "1/10")
+                }
     }
+}
+    
+
 
